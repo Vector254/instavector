5dcc7652d70f7b2f7c9f52b4133e6d26afc44dd6
Vector254 <ochrist7@gmail.com>
2020-10-16 11:46:50 +0300

    add bootstrap

--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'instavector.apps.InstavectorConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
