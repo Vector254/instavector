53c4abdd19cd8d3b7e78d1f7b39e7e5beff23125
Vector254 <ochrist7@gmail.com>
2020-10-16 16:21:53 +0300

    make migrations

--- instavector/urls.py
+++ instavector/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns=[
-    url('^$',views.index,name = 'index'),
+    url('',views.index,name = 'index'),
+    url('account/', include('django.contrib.auth.urls')),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- instavector/views.py
+++ instavector/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 
