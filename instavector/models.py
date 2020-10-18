f3f692e07fc7b276f7d139070e0f0a9bd48511ee
Vector254 <ochrist7@gmail.com>
2020-10-18 14:10:37 +0300

    add comments

--- instavector/models.py
+++ instavector/models.py
@@ -28,6 +28,10 @@ class Image(models.Model):
         images = cls.objects.all()
         return images
 
+    @property
+    def get_all_comments(self):
+        return self.comments.all()
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to='images/', default='default.png')
@@ -38,6 +42,12 @@ class Profile(models.Model):
 
 class Comment(models.Model):
     comment = models.TextField()
-    image = models.ForeignKey('Image', on_delete=models.CASCADE,null='True', blank=True )
-    user = models.ForeignKey('Profile', on_delete=models.CASCADE,null='True', blank=True )
+    image = models.ForeignKey('Image', on_delete=models.CASCADE,related_name='comments',null='True', blank=True )
+    name = models.CharField(max_length=100, blank=True)
     created = models.DateTimeField(auto_now_add=True, null=True)  
+
+    def __str__(self):
+        return f'{self.image.name} comment'
+
+    class Meta:
+        ordering = ["-pk"]
